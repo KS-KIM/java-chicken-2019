2d115bc0fc0024357f6e8af84d65b9fd5b3d5521
KSKIM <kwangsoonkim@outlook.com>
2019-12-26 17:07:35 +0900

    refactor: Category 패키지 이동 및 클래스 주석 추가

--- src/main/java/domain/menu/Category.java
+++ src/main/java/domain/menu/Category.java
@@ -1,5 +1,12 @@
-package domain;
+package domain.menu;
 
+/**
+ * 판매하는 상품의 카테고리를 의미하는 열거형
+ *
+ * @version 1.0.0
+ * @author KSKIM
+ * @since 2019-12-26
+ */
 public enum Category {
     CHICKEN("치킨"),
     BEVERAGE("음료");
@@ -8,6 +15,10 @@ public enum Category {
 
     Category(final String name) {
         this.name = name;
+    }
+
+    public boolean isChicken() {
+        return this.equals(CHICKEN);
     }
 
     @Override
